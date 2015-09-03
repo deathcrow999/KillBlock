3a39d4a81e922af0ce3e5355e504fd3df40d068c
legoboy0215 <legoboy0215weebly@gmail.com>
2015-09-03 16:05:57 +0800

    Update KillBlock.php
    WORKS!!!!

--- src/KillBlock/KillBlock.php
+++ src/KillBlock/KillBlock.php
@@ -3,40 +3,46 @@
 namespace KillBlock;
 
 use pocketmine\plugin\PluginBase;
+
 use pocketmine\event\Listener;
-use pocketmine\math\Vector3;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+
+use pocketmine\math\Vector3;
+
 use pocketmine\block\Block;
+
 use pocketmine\utils\TextFormat;
+
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\CommandSender;
-use pocketmine\event\entity\EntityDamageEvent;
+
 use pocketmine\Player;
+
 use pocketmine\level\Level;
 use pocketmine\level\Position;
 
 
 class KillBlock extends PluginBase implements Listener{
 
-public $killedByBlock = false;
+	public $killedByBlock = false;
 
-public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getLogger()->info("KillBlock is on!");
-    $this->blockname = "EMERALD_BLOCK";
-}
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TextFormat::GREEN . "KillBlock has been turned on!!!");
+	}
 
-public function onMove(PlayerMoveEvent $event){
-    $player = $event->getPlayer();
-    $x = $player->getX();
-    $y = $player->getY();
-    $z = $player->getZ();
-    $pos = new Vector3($x, $y - 1, $z);
-    $block = $player->getLevel()->getBlock($pos); //Use this method...
-        if($block->getId() === Block::get($this->blockname)->getId()){
-            $player->teleport($this->getServer()->getDefaultWorld());
+	public function onMove(PlayerMoveEvent $event){
+		$player = $event->getPlayer();
+		$x = $player->getX();
+		$y = $player->getY();
+		$z = $player->getZ();
+		$pos = new Vector3($x, $y - 1, $z);
+		$block = $player->getLevel()->getBlock($pos); //Use this method...
+        if($block->getId() === Block::get("EMERALD_BLOCK")->getId()){
+            $player->teleport(new Vector3($x, $y + 50, $z));
             $this->getLogger()->info(TextFormat::RED . "Teleported! Plugin is working.");
         }
-}
+	}
 }
