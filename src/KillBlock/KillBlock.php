a81e73612d4de6584dcfccf62733ec433304590c
Luca P. <lucapetrucci28@yahoo.it>
2015-09-03 15:32:31 +0200

    Update KillBlock.php

--- src/KillBlock/KillBlock.php
+++ src/KillBlock/KillBlock.php
@@ -6,23 +6,12 @@ use pocketmine\plugin\PluginBase;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
 
 use pocketmine\math\Vector3;
 
 use pocketmine\block\Block;
 
 use pocketmine\utils\TextFormat;
-
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\command\CommandSender;
-
-use pocketmine\Player;
-
-use pocketmine\level\Level;
-use pocketmine\level\Position;
-
 
 class KillBlock extends PluginBase implements Listener{
 
@@ -35,14 +24,10 @@ class KillBlock extends PluginBase implements Listener{
 
 	public function onMove(PlayerMoveEvent $event){
 		$player = $event->getPlayer();
-		$x = $player->getX();
-		$y = $player->getY();
-		$z = $player->getZ();
-		$pos = new Vector3($x, $y - 1, $z);
-		$block = $player->getLevel()->getBlock($pos); //Use this method...
-        if($block->getId() === Block::get("EMERALD_BLOCK")->getId()){
-            $player->teleport(new Vector3($x, $y + 50, $z));
-            $this->getLogger()->info(TextFormat::RED . "Teleported! Plugin is working.");
-        }
+		$block = $player->getLevel()->getBlock(new Vector3($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ()));
+        	if($block->getId() === Block::EMERALD_BLOCK){
+            		$player->teleport(new Vector3($player->x, $player->y + 50, $player->z));
+            		$this->getLogger()->info(TextFormat::RED . "Teleported! Plugin is working.");
+		}
 	}
 }
