5c62ff04d1a166af9b355e8fb642ee1b7e24688b
deathcrow999 <deathcrow4561@gmail.com>
2015-09-03 00:58:05 -0400

    Update KillBlock.php
    Complete rewrite from scratch

--- src/KillBlock/KillBlock.php
+++ src/KillBlock/KillBlock.php
@@ -6,8 +6,19 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\block\Block;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\CommandSender;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\Player;
+
 
 class KillBlock extends PluginBase implements Listener{
+
+public $killedByBlock = false;
+
 public function onLoad(){
   $this->getLogger()->info("KillBlock is loaded!");
 }
@@ -16,27 +27,18 @@ public function onEnable(){
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
 
-     public function onMove(PlayerMoveEvent $event){
-                $player = $event->getPlayer();
-                if($player->getLevel()->getBlockIdAt($player->getFloorX(), $player->getFloorY() -1, $player->getFloorZ()) === 138){
-                $this->killedByBlock = true;
-                $player->kill();
-                $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                }
-        }
-        
-        public function onDeath(PlayerDeathEvent $event){
-                $player = $event->getEntity();
-                if($this->killedByBlock === true){
-                        $this->killedByBlock = false;
-                        $event->setDeathMessage($player->getName()." died from standing on an emerald block");
-                }
-        }
+public function onMove(PlayerMoveEvent $ev){
+$player = $ev->getPlayer();
+  $block = $ev->getPlayer()->getLevel()->getBlock($ev->getPlayer()->floor()->subtract(0, 1));
+  if($block->getId() === Block::EMERALD_BLOCK){
+
+
+$player->teleport(new Position($x, $y, $z, $this->getServer()->getLevelByName("main")));
+  }
 }
 
 
 
-
-
+}
 
 ?>
